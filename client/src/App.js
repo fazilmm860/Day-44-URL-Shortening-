22a475d36cea4b020d998c3c0c2872416ca4764c
Mohamed Fazil M <fazilmm860@gmail.com>
2023-07-29 14:06:37 +0530

    app.js is completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,32 @@
-
+import React, { Component } from "react"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+import Header from "./components/header/Header"
+import Landing from "./components/landing/Landing";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  componentDidMount() { }
 
-    </div>
-  );
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Switch>
+              <Route path="/" component={Landing} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+        <div className="footer">
+          Built by{" "}
+          <a target="_blank" href="http://muhzi.com">
+            Muhsin.K
+          </a>© 2018
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
