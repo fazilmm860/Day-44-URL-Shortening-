895fafb0c94e5ce5daf7dfd4189d32225524bc30
Mohamed Fazil M <fazilmm860@gmail.com>
2023-07-29 14:19:57 +0530

    URL Shortening project is completed Nxt we want to complete login page

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,11 @@
+import "materialize-css/dist/css/materialize.min.css";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
+import axios from "axios";
+window.axios = axios;
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
